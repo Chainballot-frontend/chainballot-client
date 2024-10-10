a178e5122ac8c11cd8de6238c91036cbcff5737c
edehvictor <edehvictor715@gmail.com>
2024-10-10 18:26:06 +0100

    (fix: Fix deply error)

--- src/components/Landing-page/Mission/Mission.tsx
+++ src/components/Landing-page/Mission/Mission.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import { Button } from "../ui/button";
 const Mission = () => {
   return (
     <div className="p-16 flex flex-col justify-center items-center">
