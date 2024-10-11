5133d2adf6cb08a0ca19f8e8c7461740b8395750
edehvictor <edehvictor715@gmail.com>
2024-10-12 00:04:40 +0100

    deleted shadecn button component

--- src/app/admin/_components/Elections.tsx
+++ src/app/admin/_components/Elections.tsx
@@ -1,68 +1,7 @@
-// import React from 'react'
-
-// const Elections = () => {
-//   return (
-//     <div>
-//        <h1 className='text-3xl my-8 text-center text-white font-semibold'>Admin Dashboard</h1>
-//       <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-4 lg:px-16 px-3 pb-8'>
-//         { elections.map((item, i)=>(
-//           <Card className='rounded-xl text-white text-sm'>
-//             <CardHeader>
-//               <Image src={item.image} width={100} height={100} className='w-full object-cover rounded-t-xl' />
-//             </CardHeader>
-//             <CardContent className='bg-purple flex flex-col items-center justify-center gap-4 py-3 rounded-b-xl'>
-//               <h1 className='text-lg font-semibold'>{item.title}</h1>
-//               <p className='text-center'>{item.text}</p>
-//               <div className='w-full flex items-center justify-around'>
-//                 {/* <Badge className='rounded-full'>{item.status}</Badge> */}
-//                 {(() => {
-//                   let severity;
-//                   let badgeText;
-
-//                   switch (item.status) {
-//                     case 'Ongoing':
-//                       severity = 'default';
-//                       badgeText = 'Ongoing';
-//                       break;
-//                     case 'Upcoming':
-//                       severity = 'secondary';
-//                       badgeText = 'Pending';
-//                       break;
-//                     case 'Closed':
-//                       severity = 'destructive';
-//                       badgeText = 'Closed';
-//                       break;
-//                     default:
-//                       severity = 'default';
-//                       badgeText = 'Upcoming';
-//                   }
-
-//                   return (
-//                     <Badge variant={severity} className='rounded-full'>
-//                       {badgeText}
-//                     </Badge>
-//                   );
-//                 })()}
-//                 <p className=''>{item.votes} Votes</p>
-//                 <p className=''>{item.eligibles} Eligible voters</p>
-//               </div>
-//               <Button className='rounded-full bg-white text-black'>Manage Election</Button>
-//             </CardContent>
-//           </Card>
-//         )) }
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Elections
-
 import React from "react";
 import Image from "next/image";
 import { elections } from "@/Data/Election";
-
 import { Badge } from "primereact/badge";
-import { Button } from "primereact/button";
 
 const Elections = () => {
   return (
